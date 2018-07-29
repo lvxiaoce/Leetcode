e13b3c5f68ef1713291fdb05ee69f66c7103687f
lvxiaoce <lvce19950210@163.com>
2018-07-29 14:01:01 +0800

    第八十八题

--- src/problems/EightyEight.java
+++ src/problems/EightyEight.java
@@ -1,6 +1,7 @@
 package problems;
 
 /**
+ * 第八十八题：合并两个有序数组
  * 给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
  *
  * 说明:
@@ -16,39 +17,32 @@ package problems;
  */
 public class EightyEight {
     public static void main(String[] args){
-        int[] nums1 = {-1,3,0,0,0,0,0};
-        int m = 2;
-        int[] nums2 = {0,0,1,2,3};
-        int n = 5;
-//        int[] nums1 = {1,2,3,0,0,0};
-//        int m = 3;
-//        int[] nums2 = {2,5,6};
-//        int n = 3;
+//        int[] nums1 = {-1,3,0,0,0,0,0};
+//        int m = 2;
+//        int[] nums2 = {0,0,1,2,3};
+//        int n = 5;
+        int[] nums1 = {1,2,3,0,0,0};
+        int m = 3;
+        int[] nums2 = {2,5,6};
+        int n = 3;
         merge(nums1,m,nums2,n);
         for (int x:nums1) {
             System.out.print(x+",");
         }
     }
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        if(m==0){
-            for (int i = 0; i <n; i++) {
-                nums1[i] = nums2[i];
-            }
-        }else{
-            for (int i = 0; i <n; i++) {
-                for (int j = 0; j <m+n; j++) {
-                    if(i<n-1 && nums2[i]>nums2[i+1]){
-                        int x = nums2[i];
-                        nums2[i] = nums2[i+1];
-                        nums2[i+1] = x;
-                    }
-                    if(nums1[j]>nums2[i] || (nums1[j]==0 && j>nums1.length-n-1)){
-                        int s = nums1[j];
-                        nums1[j] = nums2[i];
-                        nums2[i] = s;
-                    }
-                }
-            }
+        int i = m-1,j=n-1,k=m+n-1;
+        /*
+        从两个数组末尾遍历,判断两个数哪个大,给第一个数组末尾赋值直到i和j小于0
+         */
+        while(i>=0 && j>=0){
+             nums1[k--] = nums2[j]>nums1[i]?nums2[j--]:nums1[i--];
+        }
+        /*
+        针对nums1 为一个空数组的情况
+         */
+        while (j>=0){
+            nums1[k--]=nums2[j--];
         }
     }
 }
